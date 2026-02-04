f9cd524ae1f80d0e775a1339cf717c7f991953d7
Давид <datofin23@gmail.com>
2026-02-04 09:11:26 +0300

    update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI(
     title="second-service",
@@ -6,6 +7,18 @@ app = FastAPI(
     docs_url="/second"
 )
 
-@app.get("/health")
+
+FIRST_SERVICE_URL = "http://first-service:8001"
+
+@app.get("/second/health")
 async def health():
     return "OK"
+
+@app.on_event("startup")
+async def load_first_openapi():
+    async with httpx.AsyncClient() as client:
+        r = await client.get(f"{FIRST_SERVICE_URL}/openapi.json")
+        first_openapi = r.json()
+
+    for path, methods in first_openapi["paths"].items():
+        app.openapi()["paths"][f"/first{path}"] = methods
